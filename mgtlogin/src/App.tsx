e34d804a7e13b0d8b80cf2af345f7d5cc5b6e59d
Joshila Joshi <joshila.joshi@xrdig.com>
2023-01-11 16:47:58 +0545

    login in Microsoft graph

---
+++ mgtlogin/src/App.tsx
@@ -0,0 +1,15 @@
+import { Login } from '@microsoft/mgt-react';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
